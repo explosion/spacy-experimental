00fa069aa8ac971c2ad02abd617c436867b6b732
thomashacker <EdwardSchmuhl@web.de>
2022-03-03 23:24:46 +0100

    Update sbd model architecture

--- spacy_experimental/span_boundary_detection/sbd_model.py
+++ spacy_experimental/span_boundary_detection/sbd_model.py
@@ -1,15 +1,15 @@
-from thinc.types import Floats2d
+from thinc.types import Floats2d, Floats1d
 import torch
 
 from typing import List
-from thinc.api import Model, chain, PyTorchWrapper
-from thinc.api import Maxout
+from thinc.api import Model, chain, PyTorchWrapper, Maxout, with_padded, list2ragged, with_array
 from thinc.types import Floats2d
 
 from spacy.util import registry
 from spacy.tokens import Doc
 from thinc.util import torch2xp
 from thinc.api import ArgsKwargs
+from numpy import float32
 
 
 @registry.architectures("spacy-experimental.PyTorchSpanBoundaryDetection.v1")
@@ -128,3 +128,50 @@ def _get_window_sized_tokens(input: List[Floats2d], window_size: int) -> List[Fl
     modified_vectors = torch.stack(modified_vectors)
     modified_vectors.requires_grad = True
     return modified_vectors
+
+
+@registry.architectures("spacy-experimental.SpanBoundaryDetection.v1")
+def build_boundary_model_v2(
+    tok2vec: Model[List[Doc], List[Floats2d]],
+    scorer: Model[Floats2d, Floats2d]
+) -> Model[List[Doc], Floats2d]:
+
+    logistic_layer = with_array(scorer)
+
+    model = chain(
+        tok2vec,
+        logistic_layer,
+        flattener()
+    )
+
+    model.set_ref("tok2vec", tok2vec)
+    model.set_ref("scorer", scorer)
+    model.set_ref("logistic_layer", logistic_layer)
+
+    return model
+
+
+def flattener() -> Model[Floats1d,Floats1d]:
+    def forward(model: Model[Floats1d,Floats1d], X, is_train:bool) -> Floats1d:
+        output = []
+        lengths = []
+
+        for doc in X:
+            lengths.append(len(doc))
+            for token in doc:
+                output.append(token)
+        output = model.ops.asarray(output, dtype=float32)
+
+        def backprop(Y) -> Floats2d:
+            offset = 0
+            original = []
+            for length in lengths:
+                original.append(Y[offset : offset + length])
+                offset += length
+            return original
+
+
+        return output, backprop
+
+    return Model("Flattener",forward=forward)
+
