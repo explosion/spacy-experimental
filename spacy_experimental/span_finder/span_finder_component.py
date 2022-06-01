35e9d60c7e306de5dc1f5088afe3d056a79b7c96
Adriane Boyd <adrianeboyd@gmail.com>
2022-06-01 08:38:52 +0200

    Add TODO in component

--- spacy_experimental/span_finder/span_finder_component.py
+++ spacy_experimental/span_finder/span_finder_component.py
@@ -273,6 +273,7 @@ class SpanFinder(TrainablePipe):
 
     def _get_aligned_scores(self, examples) -> Tuple[Floats2d, Floats2d]:
         """Align scores of the predictions to the references for calculating the loss"""
+        # TODO: handle 1-to-many alignments (see xfailed tests)
         reference_results = []
 
         for eg in examples:
