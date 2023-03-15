bea6f8df179ee816b81578a18a4997004f8392d5
Daniël de Kok <me@danieldk.eu>
2023-03-15 11:57:19 +0100

    Replace `torch.nn.Bilinear` by our own implementation
    Torch's bilinear layer uses the native torch `bilinear` function, which
    is seemingly less efficient (lots of kernel launches) than using
    `einsum`.

--- spacy_experimental/biaffine_parser/pytorch_bilinear.py
+++ spacy_experimental/biaffine_parser/pytorch_bilinear.py
@@ -2,6 +2,46 @@ import torch
 from torch import nn
 
 
+class Bilinear(nn.Module):
+    def __init__(
+        self, in_features: int, out_features: int, *, bias_u=True, bias_v=True
+    ):
+        super(Bilinear, self).__init__()
+
+        self.bias_u = bias_u
+        self.bias_v = bias_v
+
+        bias_u_dim = 1 if bias_u else 0
+        bias_v_dim = 1 if bias_v else 0
+
+        self.weight = nn.parameter.Parameter(
+            torch.empty(
+                out_features, in_features + bias_u_dim, in_features + bias_v_dim
+            )
+        )
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.weight)
+
+    def forward(self, u: torch.Tensor, v: torch.Tensor):
+        assert u.shape == v.shape, "Inputs to Bilinear must have the same shape"
+        assert len(u.shape) == 2, "Inputs to Bilinear must have a 2d shape"
+
+        batch_size, _ = u.shape
+
+        ones = torch.ones((batch_size, 1), dtype=u.dtype, device=u.device)
+
+        if self.bias_u:
+            u = torch.cat([u, ones], -1)
+
+        if self.bias_v:
+            v = torch.cat([v, ones], -1)
+
+        return torch.einsum("bu,bv,ouv->bo", u, v, self.weight)
+
+
 class BilinearModel(nn.Module):
     def __init__(
         self,
@@ -16,7 +56,7 @@ class BilinearModel(nn.Module):
 
         self.head = nn.Linear(nI, hidden_width)
         self.dependent = nn.Linear(nI, hidden_width)
-        self.bilinear = nn.Bilinear(hidden_width, hidden_width, nO)
+        self.bilinear = Bilinear(hidden_width, nO)
         self.activation = activation
         self._dropout = nn.Dropout(dropout)
 
