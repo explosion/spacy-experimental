4fcb7de1ccd0047a3b9635cda6f65e38198cab4a
Daniël de Kok <me@danieldk.eu>
2023-06-12 10:59:25 +0200

    with_splits: more semantic naming

--- spacy_experimental/biaffine_parser/with_splits.py
+++ spacy_experimental/biaffine_parser/with_splits.py
@@ -13,14 +13,14 @@ InnerOutT = List[Floats2d]
 def with_splits(inner: Model[InnerInT, InnerOutT]) -> Model[InT, OutT]:
     """This layer splits document representations (List[Floats2d], shape:
     [doc_len, hidden_size]) into split representations (List[Floats2d], shape:
-    [n_splits, hidden_size]) and feeds them to the inner model.
+    [split_len, hidden_size]) and feeds them to the inner model.
 
     A quirk is that the layer outputs per split rather than per doc. The reason
     is that the arc predicter will output splits with the shape [split_len,
     split_len]. Since the splits within a doc have different lengths, we cannot
-    concatenate then into a single doc representation (unless we add padding).
-    This is not an issue, since the arc predictes knows how to deal with
-    these output.
+    concatenate them into a single doc representation (unless we add padding).
+    This is not an issue, since the arc predicter knows how to deal with
+    this output.
     """
     return Model(
         "splits",
@@ -35,7 +35,7 @@ def with_splits_init(model: Model[InT, OutT], X: Optional[InT] = None, Y=None) -
         model.layers[0].initialize(X=X, Y=Y)
     else:
         X_flat, lengths = X
-        X_unflat = _unflatten_inner_flatten_outer(X_flat, lengths)
+        X_unflat = _docs_to_splits(X_flat, lengths)
         model.layers[0].initialize(X=X_unflat, Y=Y)
 
 
@@ -48,35 +48,35 @@ def with_splits_forward(
 
     X, lengths = X_lengths
 
-    X_flat = _unflatten_inner_flatten_outer(X, lengths)
+    X_flat = _docs_to_splits(X, lengths)
     Y_unflat, backprop_inner = inner(X_flat, is_train)
 
     def backprop(dY: OutT) -> InT:
         dX_unflat = backprop_inner(dY)
-        return _unflatten_outer_flatten_inner(model.ops, dX_unflat, lengths), lengths
+        return _splits_to_docs(model.ops, dX_unflat, lengths), lengths
 
     return Y_unflat, backprop
 
 
-def _unflatten_inner_flatten_outer(
-    X: List[Floats2d], lengths: List[Ints1d]
+def _docs_to_splits(
+    X_docs: List[Floats2d], docs_split_lengths: List[Ints1d]
 ) -> List[Floats2d]:
-    X_flat: List[Floats2d] = []
-    for X_inner, lengths_inner in zip(X, lengths):
-        split_offsets = lengths2offsets(lengths_inner)
-        X_flat.extend(
-            X_inner[split_offset : split_offset + split_length]
-            for split_offset, split_length in zip(split_offsets, lengths_inner)
+    X_splits: List[Floats2d] = []
+    for X_doc, doc_split_lengths in zip(X_docs, docs_split_lengths):
+        split_offsets = lengths2offsets(doc_split_lengths)
+        X_splits.extend(
+            X_doc[split_offset : split_offset + split_length]
+            for split_offset, split_length in zip(split_offsets, doc_split_lengths)
         )
-    return X_flat
+    return X_splits
 
 
-def _unflatten_outer_flatten_inner(
-    ops: Ops, X: List[Floats2d], lengths: List[Ints1d]
+def _splits_to_docs(
+    ops: Ops, X_splits: List[Floats2d], docs_split_lengths: List[Ints1d]
 ) -> List[Floats2d]:
-    r = []
-    for lengths_inner in lengths:
-        inner = X[: len(lengths_inner)]
-        X = X[len(lengths_inner) :]
-        r.append(ops.flatten(inner))
-    return r
+    X_docs = []
+    for doc_split_lengths in docs_split_lengths:
+        inner = X_splits[: len(doc_split_lengths)]
+        X_splits = X_splits[len(doc_split_lengths) :]
+        X_docs.append(ops.flatten(inner))
+    return X_docs
